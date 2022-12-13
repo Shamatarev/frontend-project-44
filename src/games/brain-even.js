f352f4e75b17bf72944dcd96abd1f19bed5ee127
Shamatarev <sh.a.v@list.ru>
2022-12-13 19:06:52 +0300

    new project

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,7 +1,7 @@
 import random from '../utils.js';
 import game from '../index.js';
 
-const description = 'Answer "yes" if number is even, otherwise answer "no".';
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const isEven = (number) => number % 2 === 0;
 
